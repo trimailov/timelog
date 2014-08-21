45bd027eb74f7a5d30e510218fed9b90b49aa1cd
Justas Trimailovas <j.trimailovas@gmail.com>
2014-08-21 20:50:21 +0300

    Add LogFile class to write/open/create log file

--- timelog.py
+++ timelog.py
@@ -1,13 +1,45 @@
-import datetime
 import click
+import datetime
+import os
+
+
+class LogFile(object):
+    def __init__(self, path=None):
+        self.path = path
+
+    def open(self, create=False):
+        try:
+            return open(self.path, 'w')
+        except IOError:
+            if create:
+                return open(self.path, 'w+')
+
+    def write(self, file, message):
+        try:
+            file.write(message)
+        except IOError:
+            file = self.open(create=True)
+            file.write(message)
+
 
 @click.command()
-@click.option('--count', default=1, help="Number of greetings.")
-@click.option('--name', prompt="Your name", help="The person to greet")
-def hello(count, name):
-    """Simple program that greets NAME for a total of COUNT times."""
-    for x in range(count):
-        click.echo('Hello %s!' % name)
+@click.option('--message',
+              prompt="Your log message",
+              help="Your work that you've been doing until now")
+def hello(message):
+    """Simple program for registering jobs at points of time"""
+    log_file = LogFile()
+    file_path = os.path.expanduser('~') + "/.timelog"
+    log_file.path = file_path
+    file = log_file.open(create=True)
+
+    time = datetime.datetime.now()
+    time = time.strftime("%Y-%m-%d %H:%M")
+    string = ': '.join((time, message))
+
+    log_file.write(file, string)
+    click.echo(message=string)
+
 
 if __name__ == "__main__":
     hello()
