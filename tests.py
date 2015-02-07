bbbcbab582807f890665604286b2abc66563417a
Justas T <j.trimailovas@gmail.com>
2015-02-07 18:54:23 +0200

    Add file creation test

---
+++ tests.py
@@ -0,0 +1,28 @@
+import unittest
+import os
+
+from timeflow import LogFile
+
+
+class TestLogFile(unittest.TestCase):
+    def setUp(self):
+        file_name = 'timeflow_test'
+        self.test_dir_path = os.path.dirname(os.path.abspath(__file__))
+        self.test_file_path = os.path.join(self.test_dir_path, file_name)
+
+    def tearDown(self):
+        # remove test file
+        os.remove(self.test_file_path)
+
+    def test_file_creates(self):
+        log_file = LogFile(path=self.test_file_path)
+
+        # check if test file does not exists before first time opening
+        self.assertFalse(os.path.exists(self.test_file_path))
+        log_file.open()
+        # check if test file is created
+        self.assertTrue(os.path.exists(self.test_file_path))
+
+
+if __name__ == "__main__":
+    unittest.main()
--- tests/tests.py
+++
@@ -1,8 +0,0 @@
-from unittest import TestCase
-
-from timelog import LogFile
-
-
-class TestLogFile(TestCase):
-    log_file = LogFile()
-    pass
