4a479b84652987e49bca360dc504b44fce7d4170
Justas T <j.trimailovas@gmail.com>
2015-02-16 21:09:13 +0200

    Remove tests after LogFile class removal
    Use click testing to test log() command

--- tests.py
+++ tests.py
@@ -1,40 +1,22 @@
 import unittest
 import os
 
-from timeflow import LogFile
+from click.testing import CliRunner
+
+import timeflow
 
 
-class TestLogFile(unittest.TestCase):
-    def setUp(self):
-        file_name = 'timeflow_test'
-        self.test_dir_path = os.path.dirname(os.path.abspath(__file__))
-        self.test_file_path = os.path.join(self.test_dir_path, file_name)
+class TestLogging(unittest.TestCase):
+    def test_log(self):
+        message = 'Arrived.'
+        time = timeflow.get_time_now()
 
-    def tearDown(self):
-        # remove test file
-        os.remove(self.test_file_path)
+        runner = CliRunner()
+        result = runner.invoke(timeflow.log, [message])
+        assert result.exit_code == 0
 
-    def test_file_creates(self):
-        log_file = LogFile(path=self.test_file_path)
-
-        # check if test file does not exist before first time opening
-        self.assertFalse(os.path.exists(self.test_file_path))
-        log_file.open()
-        # check if test file is created
-        self.assertTrue(os.path.exists(self.test_file_path))
-
-    def test_file_writes(self):
-        log_file = LogFile(path=self.test_file_path)
-
-        # check if test file does not exist before first time opening
-        self.assertFalse(os.path.exists(self.test_file_path))
-        f = log_file.open()
-
-        text = 'message'
-        f.write(text)
-
-        f = log_file.read()
-        self.assertEqual(f.read(), text)
+        log = time + ': ' + message + '\n'
+        assert result.output == log
 
 
 if __name__ == "__main__":
